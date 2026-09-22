1b003c03987955b037c76ea7df332dd7fd764049
yalmaiouff <y.almaiouff@gmail.com>
2026-09-22 11:02:56 +0300

    Delete email.js

--- email.js
+++
@@ -1,9 +0,0 @@
-const emailLink = document.getElementById("email-link");
-const isMobile = /Android|iPhone|iPad|iPod/i.test(navigator.userAgent) ||
-  (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1);
-
-if (emailLink && isMobile) {
-  const recipient = new URL(emailLink.href).searchParams.get("to");
-  emailLink.href = `ms-outlook://compose?to=${encodeURIComponent(recipient)}`;
-  emailLink.removeAttribute("target");
-}
